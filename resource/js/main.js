50fd359ff89a81ca2a8b215421b6824e2058cc2a
all_father <kumawataditya105@gmail.com>
2024-09-12 16:49:10 +0530

    UI changes

---
+++ resource/js/main.js
@@ -0,0 +1,17 @@
+var skillsContainer = document.querySelector(".skills");
+var skills = skillsContainer.children;
+console.log(skills);
+var skillDescription = document.querySelector(".about-me__skills--description");
+
+const skillsArray = Array.from(skills);
+
+skillsArray.forEach((img) => {
+  img.addEventListener("click", function () {
+    // Remove 'active' class from all images
+    skillsArray.forEach((el) => el.classList.remove("active"));
+
+    // Add 'active' class to the clicked image
+    this.classList.add("active");
+    skillDescription.innerHTML = this.dataset.info;
+  });
+});
--- resource/js/splide.js
+++ resource/js/splide.js
@@ -1,11 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-  new Splide("#js-splide", {
+  new Splide("#js-splide-projects", {
     perPage: 3,
     perMove: 1,
-    arrows: false,
     focus: "center",
+    arrows: false,
+    autoplay: "true",
+    interval: 4000,
     cover: true,
     type: "loop",
+    breakpoints: {
+      800: {
+        perPage: 2,
+        // focus: 0,
+      },
+      600: {
+        perPage: 1,
+      },
+    },
+  }).mount();
+
+  new Splide("#js-splide-about", {
+    perPage: 1,
+    perMove: 1,
+    // arrows: true,
+    cover: true,
+    type: "loop",
+    pagination: false,
+    autoplay: "true",
+    interval: 5000,
     breakpoints: {
       800: {
         perPage: 2,
