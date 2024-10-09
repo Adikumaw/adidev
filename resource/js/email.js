cf4b6af0ea91b74da71e1c30caf64f756095e8fd
all_father <kumawataditya105@gmail.com>
2024-10-09 13:16:06 +0530

    added sumbit button animation

--- resource/js/email.js
+++ resource/js/email.js
@@ -6,17 +6,34 @@
 })();
 
 window.onload = function () {
+  const submitButton = document.getElementById("submit_button");
+  const buttonText = document.querySelector(".btn-text");
   const contactForm = document.getElementById("contact-form");
+
+  // add sending indicator
+  submitButton.classList.remove("success", "failed");
+  buttonText.textContent = "Send Message";
+
   contactForm.addEventListener("submit", function (event) {
     event.preventDefault();
+    submitButton.classList.add("loading");
+    buttonText.textContent = "Sending...";
     // these IDs from the previous steps
-
-    emailjs
-      .sendForm("service_9bggxs1", "contact_form", this)
-      .then(() => {
+    emailjs.sendForm("service_9bggxs1", "contact_form", this).then(
+      () => {
         console.log("SUCCESS!");
+        submitButton.classList.remove("loading");
+        submitButton.classList.add("success");
+        buttonText.textContent = "Submitted";
         contactForm.reset();
-      })
-      .catch((err) => console.error(err));
+      },
+      (error) => {
+        console.log("FAILED...", error);
+        submitButton.classList.remove("loading");
+        submitButton.classList.add("failed");
+        buttonText.textContent = "Failed";
+        contactForm.reset();
+      }
+    );
   });
 };
